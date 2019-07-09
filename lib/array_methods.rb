5018d6453ebda451d15a368f9b0a40459964fb2a
Hanna Bergland <berglandhanna@gmail.com>
2019-07-09 20:46:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,19 @@
+require "pry"
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse 
 end
 
 def using_first(array)
-
+  array.first
+#binding.pry
 end
 
 def using_last(array)
